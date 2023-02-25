543d4532fb65188a307eaa431ecb6ea016b1c087
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-25 19:25:00 +0000

    added valid input checks

--- starter/index.js
+++ starter/index.js
@@ -13,6 +13,29 @@ const render = require("./src/page-template.js");
 // create Team array used by the generateTeam() function in page-template.js
 const team = [];
 
+
+// ----VALID INPUT CHECKS---- //
+// Throws error if there is no input
+const validateAnswer = (answer) => {
+    if (answer.length < 1) {
+        console.error("Answer needed!");
+        return false;
+    } else {
+        return true;
+    }
+};
+
+// Throws an error if no @ in the user's input
+const validateEmail = (answer) => {
+    if (answer.length < 1 || !answer.includes("@")) {
+        console.error("Please enter a valid email address");
+        return false;
+    } else {
+        return true;
+    }
+};
+
+
 // Prompts to create a team manager (this runs first)
 const managerPrompts = () =>
     inquirer.prompt([
@@ -20,21 +43,32 @@ const managerPrompts = () =>
             type: "input",
             name: "name",
             message: "What is the team manager's name?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "id",
-            message: "What is the team manager's id number?",
+            message: "What is the team manager's id?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "email",
             message: "What is the team manager's email address?",
+            validate: answer => validateEmail(answer),
         },
         {
             type: "input",
             name: "office",
             message: "What is the team manager's office number?",
+            validate: (answer) => {
+                if (answer.length < 1 || isNaN(answer)) {
+                    console.error("Please enter a number");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
     ])
     .then((answers) => {
@@ -74,21 +108,25 @@ const engineerPrompts = () =>
             type: "input",
             name: "name",
             message: "What is the engineer's name?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "id",
             message: "What is the engineer's id?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "email",
             message: "What is the engineer's email address?",
+            validate: answer => validateEmail(answer),
         },
         {
             type: "input",
             name: "github",
             message: "What is the engineer's github username?",
+            validate: answer => validateAnswer(answer),
         },
     ])
     .then((answers) => {
@@ -104,21 +142,25 @@ const internPrompts = () =>
             type: "input",
             name: "name",
             message: "What is the intern's name?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "id",
             message: "What is the intern's id?",
+            validate: answer => validateAnswer(answer),
         },
         {
             type: "input",
             name: "email",
             message: "What is the intern's email address?",
+            validate: answer => validateEmail(answer),
         },
         {
             type: "input",
             name: "school",
             message: "What is the name of the intern's school?",
+            validate: answer => validateAnswer(answer),
         },
     ])
     .then((answers) => {
